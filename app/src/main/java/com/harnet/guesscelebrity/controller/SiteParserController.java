d1eb5cb7013ba6e68033249705a23a24af0c9825
Harnet69 <harnet69@gmail.com>
2020-07-11 19:57:43 +0200

    Regex work properly

--- app/src/main/java/com/harnet/guesscelebrity/controller/SiteParserController.java
+++ app/src/main/java/com/harnet/guesscelebrity/controller/SiteParserController.java
@@ -10,12 +10,14 @@ import java.util.regex.Pattern;
 
 public class SiteParserController {
     private SourceSite sourceSite = new SourceSite("imdb.com", "https://www.imdb.com/list/ls052283250/",
-            "src=\"(.*?)\"","src=\"https://m.media-amazon.com/images/M/(.*?)\"","");
+            "img alt=\"(.*?)\"","src=\"https://m.media-amazon.com/images/M/(.*?)\"","");
     private DownloadWebContentController downloadWebContentController = new DownloadWebContentController();
 
     public SiteParserController() {
         downloadContent();
         parseContent(sourceSite.getPATTERN_NAME_LASTNAME());
+        parseContent(sourceSite.getPATTERN_PHOTO_LINK());
+//        parseContent("<img alt=\"(.*?)\">");
     }
 
     public SourceSite getSourceSite() {
@@ -25,20 +27,24 @@ public class SiteParserController {
     // downloads content of a source web page
     public void downloadContent(){
         try {
-            sourceSite.setContent(downloadWebContentController.execute(sourceSite.getLink()).get());
+            String content = downloadWebContentController.execute(sourceSite.getLink()).get();
+            //TODO print all page content
+//            System.out.println(content);
+            sourceSite.setContent(content);
         } catch (ExecutionException | InterruptedException e) {
             e.printStackTrace();
         }
     }
 
-    // TODO consistently parse a string and build Celebrity objects with name, lastmane, link to a photo and info
+    // TODO consistently parse a string and build Celebrity objects with name, lastname, link to a photo and info
     public void parseContent(String regex){
-        Pattern name_lastmane = Pattern.compile("src=\"https://m.media-amazon.com/images/M/(.*?)\"");
-        Matcher m = name_lastmane.matcher(sourceSite.getContent());
+        Log.i("Regex", "parseContent: " + regex);
+        Pattern p = Pattern.compile(regex);
+        Matcher m = p.matcher(sourceSite.getContent());
 
         while(m.find()){
         Log.i("SITE:", "parseContent: " + m.group(1));
-            System.out.println(m.group(1));
+//            System.out.println(m.group(1));
         }
     }
 }
