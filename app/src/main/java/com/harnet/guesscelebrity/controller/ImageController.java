4395a073a9ffd408729804ca95088bea8f8a5890
Harnet69 <harnet69@gmail.com>
2020-07-12 09:26:21 +0200

    Image downloads and shows correctly

--- app/src/main/java/com/harnet/guesscelebrity/controller/ImageController.java
+++ app/src/main/java/com/harnet/guesscelebrity/controller/ImageController.java
@@ -1,4 +1,18 @@
 package com.harnet.guesscelebrity.controller;
 
+import android.graphics.Bitmap;
+
+import java.util.concurrent.ExecutionException;
+
 public class ImageController {
+    private ImageDownloader imageDownloader = new ImageDownloader();
+
+    public Bitmap getImageByLink(String imageLink){
+        try {
+            return imageDownloader.execute(imageLink).get();
+        } catch (ExecutionException | InterruptedException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 }
--- app/src/main/java/com/harnet/guesscelebrity/view/MainActivity.java
+++ app/src/main/java/com/harnet/guesscelebrity/view/MainActivity.java
@@ -8,10 +8,12 @@ import android.widget.ImageView;
 
 import com.harnet.guesscelebrity.R;
 import com.harnet.guesscelebrity.controller.CelebrityController;
+import com.harnet.guesscelebrity.controller.ImageController;
 import com.harnet.guesscelebrity.model.Celebrity;
 
 public class MainActivity extends AppCompatActivity {
     private CelebrityController celebrityController;
+    private ImageController imageController;
 
     private ImageView celebrityImageView;
 
@@ -20,9 +22,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        celebrityController = CelebrityController.getInstance();
-
         celebrityImageView = findViewById(R.id.celebrity_imageView);
+
+        celebrityController = CelebrityController.getInstance();
+        imageController = new ImageController();
+
+        celebrityImageView.setImageBitmap(imageController.getImageByLink(celebrityController.getCelebrities().get(2).getPhotoLink()));
+
 
         for(Celebrity celebrity : celebrityController.getCelebrities()){
             System.out.println(celebrity.getName() + " : " +celebrity.getPhotoLink() );
