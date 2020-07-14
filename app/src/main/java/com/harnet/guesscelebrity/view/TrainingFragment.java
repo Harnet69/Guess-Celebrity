712cc0bc180de09d35dfbcef49c65170b4a73077
Harnet69 <harnet69@gmail.com>
2020-07-14 21:05:11 +0200

    Training mode works correctly

--- app/src/main/java/com/harnet/guesscelebrity/view/TrainingFragment.java
+++ app/src/main/java/com/harnet/guesscelebrity/view/TrainingFragment.java
@@ -19,6 +19,7 @@ import android.widget.TextView;
 
 import com.harnet.guesscelebrity.R;
 import com.harnet.guesscelebrity.controller.CelebrityController;
+import com.harnet.guesscelebrity.controller.GameController;
 import com.harnet.guesscelebrity.controller.ImageController;
 import com.harnet.guesscelebrity.model.Celebrity;
 
@@ -30,9 +31,11 @@ public class TrainingFragment extends Fragment {
 
     private ImageView celebrityImageView;
     private TextView celebrityNameTextView;
+    private Button gotItBtn;
     private EditText messageEditText;
-    private Button sendButton;
     private OnMessageSendListener onMessageSendListener;
+
+    private int numOfUnGuessed;
 
     private List<Celebrity> unGuessedCelebrities;
 
@@ -51,16 +54,35 @@ public class TrainingFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        imageController = new ImageController();
         unGuessedCelebrities = CelebrityController.getInstance().getListOfCelebritiesByGuess(false);
+        imageController = new ImageController();
 
         View view =  inflater.inflate(R.layout.fragment_training, container, false);
         celebrityImageView = view.findViewById(R.id.celebrity_imageView);
         celebrityNameTextView = view.findViewById(R.id.celebrity_name_textView);
+        gotItBtn = view.findViewById(R.id.got_it_btn);
 
-        //TODO test the first unguessed celebrity
-        celebrityImageView.setImageBitmap(imageController.getImageByLink(CelebrityController.getInstance().getCelebrityByName(unGuessedCelebrities.get(0).getName()).getPhotoLink()));
-        celebrityNameTextView.setText(unGuessedCelebrities.get(0).getName());
+        celebrityImageView.setImageBitmap(imageController.getImageByLink(CelebrityController.getInstance().getCelebrityByName(unGuessedCelebrities.get(numOfUnGuessed).getName()).getPhotoLink()));
+        celebrityNameTextView.setText(unGuessedCelebrities.get(numOfUnGuessed).getName());
+        gotItBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(numOfUnGuessed < unGuessedCelebrities.size()-1){
+                    numOfUnGuessed++;
+                    Celebrity celebrity = CelebrityController.getInstance().getCelebrityByName(unGuessedCelebrities.get(numOfUnGuessed).getName());
+                    imageController = new ImageController();
+                    celebrityImageView.setImageBitmap(imageController.getImageByLink(celebrity.getPhotoLink()));
+                    celebrityNameTextView.setText(unGuessedCelebrities.get(numOfUnGuessed).getName());
+                    CelebrityController.getInstance().getCelebrityByName(celebrity.getName()).setGuessed(null); //reset celebrity guess status
+                }else{
+                    //TODO goto GameFragment
+                }
+            }
+        });
+
+        //TODO add loop through unguessed celebrities
+//        celebrityImageView.setImageBitmap(imageController.getImageByLink(CelebrityController.getInstance().getCelebrityByName(unGuessedCelebrities.get(0).getName()).getPhotoLink()));
+//        celebrityNameTextView.setText(unGuessedCelebrities.get(0).getName());
 
         return view;
     }
